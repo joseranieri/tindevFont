76765ec7cf2aa9812c6edce5770a0f61843d8a79
joseranieri <jose.ranieri22@gmail.com>
2019-10-29 13:54:40 -0300

    Finalizada versao web

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,18 +1,18 @@
-import React, {useEffect, useState} from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/tindev.svg';
 import api from '../services/api';
 import dislike from '../assets/dislike.svg';
 import like from '../assets/like.svg';
 import './Main.css';
 
-export default function Main({ match }){
+export default function Main({ match }) {
     const [users, setUsers] = useState([]);
 
-    useEffect(()=>{
-        async function loadUsers(){
-            const response = await api.get('/devs',{
-                headers:{
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await api.get('/devs', {
+                headers: {
                     user: match.params.id,
                 }
             })
@@ -20,40 +20,53 @@ export default function Main({ match }){
             setUsers(response.data);
         };
         loadUsers();
-    },[match.params.id]);
+    }, [match.params.id]);
 
-    async function handleLike(id){
-        console.log('like ', id);
+    async function handleLike(id) {
+        await api.post(`/devs/${id}/likes`, null, {
+            headers: { user: match.params.id },
+        })
+
+        setUsers(users.filter(user => user._id !== id));
     }
 
-    async function handleDislike(id){
+    async function handleDislike(id) {
         await api.post(`/devs/${id}/dislikes`, null, {
-            headers:{user: match.params.id},
+            headers: { user: match.params.id },
         })
+
+        setUsers(users.filter(user => user._id !== id));
     }
 
     return (
         <div className="main-container">
-            <img src={logo} alt="Tindev"/>
-            <ul>
-                {users.map(user => (
-                    <li key={user._id}>
-                        <img src={user.avatar} alt={user.name}/>
-                        <footer>
-                            <strong>{user.name}</strong>
-                            <p>{user.bio}</p>
-                        </footer>
-                        <div className="buttons">
-                            <button type="button" onClick={() => handleDislike(user._id)}>
-                                <img src={dislike} alt="Dislike"/>
-                            </button>
-                            <button type="button" onClick={() => handleLike(user._id)}>
-                                <img src={like} alt="Like"/>
-                            </button>
-                        </div>
-                    </li>
-                ))}                                
-            </ul>
+            <Link to="/">
+                <img src={logo} alt="Tindev" />
+            </Link>
+            {users.length > 0 ? (
+                <ul>
+                    {users.map(user => (
+                        <li key={user._id}>
+                            <img src={user.avatar} alt={user.name} />
+                            <footer>
+                                <strong>{user.name}</strong>
+                                <p>{user.bio}</p>
+                            </footer>
+                            <div className="buttons">
+                                <button type="button" onClick={() => handleDislike(user._id)}>
+                                    <img src={dislike} alt="Dislike" />
+                                </button>
+                                <button type="button" onClick={() => handleLike(user._id)}>
+                                    <img src={like} alt="Like" />
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                    <div className="empty">Acabou :( </div>
+                )}
+
         </div>
     )
 };
